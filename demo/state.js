076715c491bee7a95139d210548bd5abbeb4799d
sergeyt <stodyshev@gmail.com>
2015-11-24 21:05:27 +0600

    updated make-reducer

--- demo/state.js
+++ demo/state.js
@@ -8,14 +8,14 @@ const initialState = {
 
 export const reducer = makeReducer(initialState);
 
-export const addMessage = reducer.add('ADD_MESSAGE', (state, msg) => {
+export const addMessage = reducer.on('ADD_MESSAGE', (state, msg) => {
 	return {
 		...state,
 		messages: [...state.messages, msg],
 	};
 });
 
-export const removeMessage = reducer.add('REMOVE_MESSAGE', (state, msg) => {
+export const removeMessage = reducer.on('REMOVE_MESSAGE', (state, msg) => {
 	return {
 		...state,
 		messages: state.messages.filter(m => m.id !== msg.id),
