12b8519762ae97a99fdab695f4b8f63409d72f93
sergeyt <stodyshev@gmail.com>
2015-12-12 02:05:25 +0600

    use babel-plugin-react-transform

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ const autoprefixer = require('autoprefixer');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 module.exports = {
-	devtool: 'source-map',
+	devtool: 'cheap-module-eval-source-map',
 	entry: [
 		'webpack-hot-middleware/client',
 		'./demo/index',
