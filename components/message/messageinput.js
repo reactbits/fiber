20588f06cdd27983d54107d921ce4b12011b6757
sergeyt <stodyshev@gmail.com>
2016-01-07 15:04:33 +0600

    reply input with contextual help

--- components/message/messageinput.js
+++ components/message/messageinput.js
@@ -1,11 +1,41 @@
 import React, { Component, PropTypes } from 'react';
 import ReactDOM from 'react-dom';
 import classNames from 'classnames';
+import PopoverClass from 'react-popover';
 import style from './style';
 import _ from 'lodash';
 
+const popover = React.createFactory(PopoverClass);
+
+function helpContent() {
+	const quote = '> ';
+	const monospaced = '`';
+	return (
+		<div>
+			<div className={style.help_format}>
+				<span><b>*bold*</b></span><br/>
+				<span><i>_italic_</i></span><br/>
+				<span>{quote}{'quoted'}</span><br/>
+				<span className={style.monospaced}>{monospaced}{'monospaced'}{monospaced}</span><br/>
+				<span>[title](link)</span><br/>
+			</div>
+			<div className={style.help_code}>
+				<span>{'```js'}</span><br/>
+				<span>javascript code</span><br/>
+				<span>{'```'}</span><br/>
+			</div>
+			<div className={style.help_post}>
+				<em>ctrl</em>
+				{' + '}
+				<em>enter</em>
+				<span>&nbsp;post</span>
+			</div>
+		</div>
+	);
+}
+
 // TODO render user avatar
-// TODO configure submit shortcut, alt-enter is default
+// TODO configure submit shortcut, ctrl-enter is default
 
 export class MessageInput extends Component {
 	static propTypes = {
@@ -18,10 +48,34 @@ export class MessageInput extends Component {
 		focused: false,
 	};
 
+	constructor(props) {
+		super(props);
+		this.state = {
+			helpVisible: false,
+		};
+		this.onFocus = this.onFocus.bind(this);
+		this.onBlur = this.onBlur.bind(this);
+	}
+
 	componentDidMount() {
+		const input = $(ReactDOM.findDOMNode(this)).find('.message-input');
 		if (!!this.props.focused) {
-			$(ReactDOM.findDOMNode(this)).find('.message-input').focus();
+			input.focus();
 		}
+		input.focus(this.onFocus).blur(this.onBlur);
+	}
+
+	componentWillUnmount() {
+		const input = $(ReactDOM.findDOMNode(this)).find('.message-input');
+		input.off('focus').off('blur');
+	}
+
+	onFocus() {
+		$(ReactDOM.findDOMNode(this)).addClass(style.focused);
+	}
+
+	onBlur() {
+		$(ReactDOM.findDOMNode(this)).removeClass(style.focused);
 	}
 
 	render() {
@@ -35,17 +89,45 @@ export class MessageInput extends Component {
 				}
 			}
 			const input = $(e.target);
-			if (e.altKey && e.which === 13) {
+			if (e.ctrlKey && e.which === 13) {
 				const text = input.val();
 				if (!text) return;
 				input.val('');
 				props.submit(text);
 			}
 		};
+
+		const showHelp = (e) => {
+			e.preventDefault();
+			e.stopPropagation();
+			this.setState({ helpVisible: true });
+			return false;
+		};
+
+		const showHelpButton = <a className={style.show_help} onMouseDown={showHelp}>?</a>;
+
+		const helpProps = {
+			className: style.help,
+			isOpen: this.state.helpVisible,
+			preferPlace: 'below',
+			place: 'below',
+			onOuterAction: () => this.setState({ helpVisible: false }),
+			body: helpContent(),
+		};
+
+		const help = popover(helpProps, showHelpButton);
+
 		return (
-			<div>
+			<div className={style.reply_form}>
+				{showHelpButton}
+				{help}
 				<textarea className={className} placeholder="Reply..." onKeyUp={onKeyUp}>
 				</textarea>
+				<div className={style.reply_controls}>
+					<a className={style.upload_button} data-toggle="tooltip" title="Upload images">
+						<i className="ion-camera"/>
+					</a>
+				</div>
 			</div>
 		);
 	}
