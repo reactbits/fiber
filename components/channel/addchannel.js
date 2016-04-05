d6b4b1a898b90f4890bdff73070fc1d95465c1af
sergeyt <stodyshev@gmail.com>
2016-04-06 00:32:13 +0600

    reduced new channel form

--- components/channel/addchannel.js
+++ components/channel/addchannel.js
@@ -2,78 +2,49 @@ import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import { Flex, Box } from 'reflexbox';
 import { Modal, Button } from 'react-bootstrap';
-import Input from 'reactbits-input';
+import { Form, Input } from 'reactbits-input';
 import style from './style';
-import _ from 'lodash';
 
 class Dialog extends Component {
 	constructor(props) {
 		super(props);
 		this.state = {
 			show: true,
-			name: '',
-			description: '',
-			hiddenValidationState: true,
-			validationState: {},
 		};
-	}
-
-	isValid() {
-		return _.reduce(this.state.validationState, (p, v) => p && v, true);
 	}
 
 	render() {
 		const close = () => this.setState({ show: false });
-		const changeHandler = name => (value, isValid) => {
-			const vs = _.assign(this.state.validationState, { [name]: isValid });
-			this.setState({
-				[name]: value,
-				validationState: vs,
-				hiddenInvalidState: true,
-			});
-		};
 		const inputs = {
 			name: {
 				name: 'name',
 				placeholder: 'Channel name',
-				value: this.state.name,
 				required: true,
-				onChange: changeHandler('name'),
-				hiddenInvalidState: this.state.hiddenInvalidState,
 			},
 			description: {
 				name: 'description',
 				placeholder: 'Description',
-				value: this.state.description,
-				onChange: changeHandler('description'),
-				hiddenInvalidState: this.state.hiddenInvalidState,
 			},
-		};
-		const submit = () => {
-			if (!this.isValid()) {
-				this.setState({ hiddenInvalidState: false });
-				return;
-			}
-			this.setState({ hiddenInvalidState: true });
-			this.props.submit(_.pick(this.state, 'name', 'description'));
 		};
 		return (
 			<Modal dialogClassName={style.new_channel_dialog} show={this.state.show} onHide={close}>
-				<Modal.Header closeButton>
-					<Modal.Title>Create new channel</Modal.Title>
-				</Modal.Header>
-				<Modal.Body>
-					<Flex>
-						<Box col={12}>
-							<Input {...inputs.name} />
-							<Input {...inputs.description} />
-						</Box>
-					</Flex>
-				</Modal.Body>
-				<Modal.Footer>
-					<Button bsStyle="primary" onClick={submit}>Create</Button>
-					<Button onClick={close}>Cancel</Button>
-				</Modal.Footer>
+				<Form onSubmit={this.props.submit}>
+					<Modal.Header closeButton>
+						<Modal.Title>Create new channel</Modal.Title>
+					</Modal.Header>
+					<Modal.Body>
+						<Flex>
+							<Box col={12}>
+								<Input {...inputs.name} />
+								<Input {...inputs.description} />
+							</Box>
+						</Flex>
+					</Modal.Body>
+					<Modal.Footer>
+						<Button type="submit" bsStyle="primary">Create</Button>
+						<Button onClick={close}>Cancel</Button>
+					</Modal.Footer>
+				</Form>
 			</Modal>
 		);
 	}
