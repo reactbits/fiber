29eb02751f9c2264e211feae117c6f1183dd7c70
sergeyt <stodyshev@gmail.com>
2016-04-13 23:47:50 +0600

    use http-proxy-middleware

--- devserver.js
+++ devserver.js
@@ -1,14 +1,8 @@
-const startServer = require('react-devpack').startServer;
+const dev = require('react-devpack');
+const proxyMiddleware = require('http-proxy-middleware');
 
-const port = 8000 || process.env.PORT;
-
-function extendApp(app) {
-	// proxying of api requests
-	const makeProxy = require('apiproxy');
-	app.all('/api/*', makeProxy({ port: 3000, serverPort: port }));
-}
-
-startServer({
-	port: port, // eslint-disable-line
-	extendApp: extendApp, // eslint-disable-line
+dev.startServer({
+	extendApp(app) {
+		app.use(proxyMiddleware('http://localhost:3000/api'));
+	},
 });
