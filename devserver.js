d6d0e21c61abedf8af82a4d9f4d233720b283d79
sergeyt <stodyshev@gmail.com>
2016-04-21 21:55:52 +0600

    use proxy option

--- devserver.js
+++ devserver.js
@@ -1,8 +1,5 @@
 const dev = require('react-devpack');
-const proxyMiddleware = require('http-proxy-middleware');
 
 dev.startServer({
-	extendApp(app) {
-		app.use(proxyMiddleware('http://localhost:3000/api'));
-	},
+	proxy: 'http://localhost:3000/api',
 });
